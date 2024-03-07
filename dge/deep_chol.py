85121139445e9739eedfe672037fb1bf59044c8c
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-03-07 16:12:55 +0000

    removing ddim pytorch

--- dge/deep_chol.py
+++ dge/deep_chol.py
@@ -1,7 +1,40 @@
 from dataclasses import dataclass
 
 import flax.linen as nn
+import jax.numpy as jnp
+from jax import Array
 
 
+@dataclass
 class DeepChol(nn.Module):
-    model: nn.Module
+    r"""`DeepChol` learns to approximate the function $f_\theta:(\mathbf{z},\text{var},\text{ls})\to\mathbf{Lz}$.
+
+    Note that $\mathbf{L}$, and thus $\mathbf{Lz}$, is conditional on the kernel
+    and the locations where it is sampled. If we label the kernel $\mathcal{K}$,
+    and the locations where it is sampled $\mathbf{x}$, we can write the function
+    more accurately as:
+
+    $f_{\theta\mid\mathcal{K},\mathbf{x}}(\mathbf{z},\text{var},\text{ls})\approx\langle\text{Cholesky}(\mathcal{K}(\mathbf{x},\mathbf{x}\mid\text{var},\text{ls})),\mathbf{z}\rangle$
+
+    The decoder submodule can be any neural network whose output
+    size is `num_locations`.
+
+    Args:
+        z: Standard normal samples with one `z` for each location. The first
+            dimension is assumed to be the batch dimension.
+        var: Variance to use when calculating `f`.
+        ls: lengthscale to use when calculating `f`.
+
+    Returns:
+        $\mathbf{f}$, an approximation of $\mathbf{Lz}$.
+    """
+
+    decoder: nn.Module
+
+    @nn.compact
+    def __call__(self, z: Array, var: float, ls: float):
+        batch_size = z.shape[0]
+        var = jnp.full((batch_size, 1), var)
+        ls = jnp.full((batch_size, 1), ls)
+        x = jnp.hstack([z, var, ls])
+        return self.decoder(x)
