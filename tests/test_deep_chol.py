98011d0167cbf305ca2fe88e845876455e6805f6
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-03-15 13:55:48 +0000

    removing incomplete unit test

--- tests/test_deep_chol.py
+++
@@ -1,8 +0,0 @@
-from dge import DeepChol
-from sps.gp import cholesky
-from sps.kernels import rbf
-from sps.utils import build_grid
-
-
-def test_deep_chol():
-    locs = build_grid([{"start": 0, "stop": 1, "num": 32}])
