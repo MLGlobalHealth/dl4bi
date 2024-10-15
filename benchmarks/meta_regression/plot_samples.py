c2a9d08f362d179e8acf11dfb86ed3ab8485fce5
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-10-15 16:34:15 +0100

    updates

--- benchmarks/meta_regression/plot_samples.py
+++ benchmarks/meta_regression/plot_samples.py
@@ -1,7 +1,9 @@
 import re
 from glob import glob
+from math import prod
 
 import hydra
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from jax import random
 from omegaconf import DictConfig
@@ -87,8 +89,55 @@ def plot_1d_gp_samples(
         plt.clf()
 
 
-def plot_2d_gp_samples(rng, cfg, **kwargs):
-    pass
+def plot_2d_gp_samples(
+    cfg,
+    only_regex,
+    exclude_regex,
+    num_ctx=128,
+    num_samples=16,
+):
+    rng = random.key(cfg.seed)
+    cfg.data.batch_size = 1
+    cfg.data.num_ctx.min = 0  # use only grid points in dataloader
+    cfg.data.num_ctx.max = 0
+    cfg.kernel.kwargs.ls.kwargs.dist = "fixed"
+    dir = f"results/gp/{cfg.data.name}/{cfg.kernel.kwargs.kernel.func}/{cfg.seed}/"
+    ckpt = {}
+    for p in glob(dir + "*.ckpt"):
+        if only_regex.match(str(p)) and not exclude_regex.match(str(p)):
+            state, tmp_cfg = load_ckpt(p)
+            ckpt[cfg_to_run_name(tmp_cfg)] = state
+    num_rows, num_cols = len(ckpt), 4
+    fig_size = (6 * num_cols, 4 * num_rows)
+    L = prod([axis.num for axis in cfg.data.s])
+    for i in range(num_samples):
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=fig_size)
+        rng_data, rng_permute, rng_dropout, rng_extra, rng = random.split(rng, 5)
+        for col in range(num_cols):
+            dataloader = build_gp_dataloader(cfg.data, cfg.kernel)
+            _, _, _, s, f, *_ = next(dataloader(rng_data))
+            permute_idx = random.choice(rng_permute, L, (L,), replace=False)
+            inv_permute_idx = jnp.argsort(permute_idx)
+            # permute the order and select the first valid_lens_ctx for context
+            s_permuted = s[:, permute_idx, :]
+            f_permuted = f[:, permute_idx, :]
+            valid_lens_ctx = jnp.array([num_ctx])
+            for row, run_name in enumerate(ckpt):
+                state = ckpt[run_name]
+                f_mu, f_std, *_ = state.apply_fn(
+                    {"params": state.params, **state.kwargs},
+                    s_permuted,
+                    f_permuted,
+                    s_permuted,
+                    valid_lens_ctx,
+                    rngs={"dropout": rng_dropout, "extra": rng_extra},
+                )
+                plt.sca(axs[row, col])
+                plt.clf()
+                # TODO(danj): PLOT
+        fig.tight_layout()
+        fig.savefig(dir + f"comparison_sample_{i+1}.png", dpi=150)
+        plt.clf()
 
 
 def plot_2d_img_samples(rng, cfg, **kwargs):
