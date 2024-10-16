f912455fd69bdda3da05474d797e3f79a722385c
Jhonathan Navott <jhonathan.navott@gmail.com>
2024-10-16 11:41:17 +0100

    Fix: Code review changes

--- benchmarks/meta_regression/plot_samples.py
+++ benchmarks/meta_regression/plot_samples.py
@@ -21,21 +21,12 @@ from dl4bi.meta_regression.train_utils import (
 )
 
 
-def get_ckpts(dir: str, only_regex: re.Pattern, exclude_regex: re.Pattern):
-    ckpt = {}
-    for p in glob(dir + "*.ckpt"):
-        if only_regex.match(str(p)) and not exclude_regex.match(str(p)):
-            state, tmp_cfg = load_ckpt(p)
-            ckpt[cfg_to_run_name(tmp_cfg)] = state
-    return ckpt
-
-
 @hydra.main(config_name="default", version_base=None)
 def main(cfg: DictConfig):
     gp_tasks = re.compile("Gaussian Processes", re.IGNORECASE)
     img_tasks = re.compile("MNIST|CelebA|Cifar", re.IGNORECASE)
     only_regex = re.compile(cfg.get("only", ".*"), re.IGNORECASE)
-    exclude_regex = re.compile(cfg.get("exclude", r"a^"), re.IGNORECASE)
+    exclude_regex = re.compile(cfg.get("exclude", r"^$"), re.IGNORECASE)
     num_samples = cfg.get("num_samples", None)
     if gp_tasks.match(cfg.project):
         if "1d" in cfg.data.name:
@@ -60,7 +51,7 @@ def plot_1d_gp_samples(
     cfg.data.num_ctx.max = num_ctx
     cfg.kernel.kwargs.ls.kwargs.dist = "fixed"
     dir = f"results/gp/{cfg.data.name}/{cfg.kernel.kwargs.kernel.func}/{cfg.seed}/"
-    ckpt = get_ckpts(dir, only_regex, exclude_regex)
+    ckpt = load_ckpts(dir, only_regex, exclude_regex)
     num_rows, num_cols = len(ckpt), len(lengthscales)
     fig_size = (6 * num_cols, 4 * num_rows)
     for i in range(num_samples):
@@ -89,6 +80,7 @@ def plot_1d_gp_samples(
                     f_mu[..., 0],
                     f_std[..., 0],
                 )
+                # NOTE: only set title and labels for upper-left most axes
                 if row == 0:
                     axs[row, col].set_title(f"ls={ls:0.1f}")
                 if col == 0:
@@ -151,12 +143,6 @@ def plot_2d_gp_samples(
         plt.clf()
 
 
-def get_task_image_dims(task: str):
-    channels = 3 if task != "MNIST" else 1
-    img_ax_size = 32 if task != "MNIST" else 28
-    return channels, img_ax_size
-
-
 def get_image_task_dataloader(cfg: DictConfig, num_ctx: int, img_ax_size: int):
     L = img_ax_size * img_ax_size
     build_data_loader = {
@@ -165,19 +151,24 @@ def get_image_task_dataloader(cfg: DictConfig, num_ctx: int, img_ax_size: int):
         "Cifar_10": build_dataloaders_cifar_10,
     }[cfg.project]
     return build_data_loader(
-        batch_size=1, num_ctx_max=num_ctx, num_ctx_min=num_ctx, num_test_max=L
+        batch_size=1,
+        num_ctx_max=num_ctx,
+        num_ctx_min=num_ctx,
+        num_test_max=L,
     )[-1]
 
 
 def plot_2d_img_samples(cfg, only_regex, exclude_regex, num_samples=16, num_ctx=50):
     dir = f"results/{cfg.project.lower()}/{cfg.seed}/"
-    ckpt = get_ckpts(dir, only_regex, exclude_regex)
-    channels, img_ax_size = get_task_image_dims(cfg.project)
-    task_dataloader = get_image_task_dataloader(cfg, num_ctx, img_ax_size)
-    for example in range(num_samples):
+    ckpt = load_ckpts(dir, only_regex, exclude_regex)
+    num_channels, fig_size = {"MNIST": (1, 28), "CelebA": (3, 32), "Cifar_10": (3, 32)}[
+        cfg.project
+    ]
+    task_dataloader = get_image_task_dataloader(cfg, num_ctx, fig_size)
+    for i in range(num_samples):
         min_boundary, max_boundary = 10, -10
-        rng = random.key(example)
-        rng_extra, rng_sample = random.split(rng, 2)
+        rng = random.key(i)
+        rng_extra, rng_sample = random.split(rng)
         (
             s_ctx,
             f_ctx,
@@ -196,11 +187,9 @@ def plot_2d_img_samples(cfg, only_regex, exclude_regex, num_samples=16, num_ctx=
             num_cols,
             figsize=(9 * num_cols, 6 * num_rows),
         )
-
         model_preds = {}
         for row_idx, model_name in enumerate(ckpt):
             state = ckpt[model_name]
-
             f_mu, f_std, *_ = state.apply_fn(
                 {"params": state.params, **state.kwargs},
                 s_ctx,
@@ -213,19 +202,16 @@ def plot_2d_img_samples(cfg, only_regex, exclude_regex, num_samples=16, num_ctx=
             model_preds[model_name] = (f_mu, f_std)
             min_boundary = min(min_boundary, f_std.min())
             max_boundary = max(max_boundary, f_std.max())
-
         sm = mpl.cm.ScalarMappable(
             cmap="Spectral_r",
             norm=mpl.colors.Normalize(vmin=min_boundary, vmax=max_boundary),
         )
         sm.set_array([])
-
         for row_idx, model_name in enumerate(ckpt):
             f_mu, f_std = model_preds[model_name]
-
             plot_img(
                 0,
-                (img_ax_size, img_ax_size, channels),
+                (fig_size, fig_size, num_channels),
                 f_ctx[0, :num_ctx],
                 f_mu[0],
                 f_std[0],
@@ -233,23 +219,31 @@ def plot_2d_img_samples(cfg, only_regex, exclude_regex, num_samples=16, num_ctx=
                 inv_permute_idx,
                 axs[row_idx],
             )
-
+            # NOTE: unset titles by row, unless its the first row, and increase tick size
             for col_idx in range(num_cols):
+                axs[row_idx, col_idx].tick_params(axis="both", labelsize=16)
                 if row_idx != 0:
                     axs[row_idx, col_idx].set_title("")
                 else:
                     axs[row_idx, col_idx].set_title(
-                        axs[row_idx, col_idx].get_title(), fontsize=20
+                        axs[row_idx, col_idx].get_title(), fontsize=24
                     )
-
-            axs[row_idx, 0].set_ylabel(f"{model_name}", fontsize=20)
-
+            axs[row_idx, 0].set_ylabel(f"{model_name}", fontsize=24)
         cbar_ax = fig.add_axes([0.05, 0.3, 0.02, 0.4])
         fig.colorbar(sm, cax=cbar_ax)
-
         fig.tight_layout(rect=[0.05, 0, 1, 1])
-        fig.savefig(dir + f"comparison_sample_{example+1}.png", dpi=150)
+        fig.savefig(dir + f"comparison_sample_{i+1}.png", dpi=150)
         plt.clf()
+
+
+def load_ckpts(dir: str, only_regex: re.Pattern, exclude_regex: re.Pattern):
+    """Loads all checkpoints in a given base dir"""
+    ckpt = {}
+    for p in glob(dir + "*.ckpt"):
+        if only_regex.match(str(p)) and not exclude_regex.match(str(p)):
+            state, tmp_cfg = load_ckpt(p)
+            ckpt[cfg_to_run_name(tmp_cfg)] = state
+    return ckpt
 
 
 if __name__ == "__main__":
