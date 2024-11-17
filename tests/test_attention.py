e750e521fcf480d7a9a28523ed94b6ed9720934c
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-11-17 16:21:29 +0000

    adding benchmarking test

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -189,7 +189,9 @@ def test_scan_attention_speed():
     except XlaRuntimeError:  # OOM
         t_true_diff = 1e6
 
-    factor = 5
+    max_t, factor = 2e-4, 5
+    # NOTE: can use the following assert for benchmarking
+    assert t_scan_diff < max_t, f"Scan takes longer than {max_t}s!"
     assert t_scan_diff < factor * t_true_diff, f"Scan is more than {factor}x slower!"
 
 
