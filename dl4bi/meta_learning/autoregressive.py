9373b6bf7369bebc94c0937cfcf721514f61403c
Piotr Grynfelder <piotr@grynfelder.dev>
2025-02-14 23:14:12 +0000

    bugfixes

--- dl4bi/meta_learning/autoregressive.py
+++ dl4bi/meta_learning/autoregressive.py
@@ -52,17 +52,16 @@ def build_gp_dataloader(data: DictConfig, kernel: DictConfig):
 def sample_diagonal(
         rng: jax.Array,
         apply: Apply,
-        s_ctx: jax.Array,  # [L_ctx, D_s]
-        f_ctx: jax.Array,  # [L_ctx, D_f]
-        s_test: jax.Array,  # [L_test, D_s]
+        s_ctx: jax.Array,  # [L_ctx, D]
+        f_ctx: jax.Array,  # [L_ctx, 1]
+        s_test: jax.Array,  # [L_test, D]
         B: int,  # how many paths to sample
 ):
     L_test, _ = s_test.shape
-    _, D_f = f_ctx.shape
 
     # precompute the randomness
-    normals = random.normal(rng, (B, L_test, D_f))
-    log_densities = jnp.sum(jax.scipy.stats.norm.logpdf(normals), axis=0)
+    normals = random.normal(rng, (B, L_test, 1))
+    log_densities = jnp.sum(jax.scipy.stats.norm.logpdf(normals), axis=1)
 
     s_ctx = jnp.expand_dims(s_ctx, 0)
     f_ctx = jnp.expand_dims(f_ctx, 0)
@@ -80,9 +79,9 @@ def sample_diagonal(
 def _sample_autoreg(
     rng: jax.Array,
     apply: Apply,
-    s_ctx: jax.Array,  # [B, L_ctx, D_s]
-    f_ctx: jax.Array,  # [B, L_ctx, D_f]
-    s_test: jax.Array,  # [B, L_test, D_s]
+    s_ctx: jax.Array,  # [B, L_ctx, D]
+    f_ctx: jax.Array,  # [B, L_ctx, 1]
+    s_test: jax.Array,  # [B, L_test, D]
 ):
     B, L_test, _ = s_test.shape
     _, L_ctx, _ = s_ctx.shape
@@ -183,9 +182,9 @@ def closest_first(
 def sample_autoreg(
     rng: jax.Array,
     apply: Apply,
-    s_ctx: jax.Array,  # [L_ctx, D_s]
-    f_ctx: jax.Array,  # [L_ctx, D_f]
-    s_test: jax.Array,  # [L_test, D_s]
+    s_ctx: jax.Array,  # [L_ctx, D]
+    f_ctx: jax.Array,  # [L_ctx, 1]
+    s_test: jax.Array,  # [L_test, D]
     B: int,  # how many paths to sample
     random: bool = False, # whether to permute s_test randomly
 ):
@@ -196,8 +195,8 @@ def sample_autoreg(
     if not random:
         return _sample_autoreg(rng, apply, s_ctx, f_ctx, s_test)
     else:
-        _, L_test, D_s = s_test.shape
-        _, _, D_f = f_ctx.shape
+        _, L_test, D = s_test.shape
+
         rng, rng_perm = random.split(rng)
 
         # Locations for each path are permuted independently.
@@ -207,8 +206,8 @@ def sample_autoreg(
         idx_inv = jax.vmap(invert_permutation)(idx)
 
         # idx needs to match dimension of array in take_along_axis
-        idx = jnp.repeat(idx[:, :, None], D_s, axis=2)
-        idx_inv = jnp.repeat(idx_inv[:, :, None], D_f, axis=2)
+        idx = jnp.repeat(idx[:, :, None], D, axis=2)
+        idx_inv = idx_inv[:, :, None]
 
         paths, log_densities = _sample_autoreg(
             rng,
