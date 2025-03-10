60d4e48e90f1389e628a34f4960e0bd46918eb9e
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-03-10 22:01:20 +0000

    fixing bug

--- dl4bi/meta_learning/data/spatial.py
+++ dl4bi/meta_learning/data/spatial.py
@@ -96,7 +96,6 @@ def _batch(
     else:
         s, f = map(S_to_L, (s, f))
         s, f, inv_permute_idx = permute_L_in_BLD(rng_p, [s, f])
-        inv_permute_idx = jnp.arange(s.shape[1])
         args = batch_BLD(rng_b, [s, f], *batch_args)
         s_ctx, f_ctx, m_ctx, *rest = args
         args = (None, s_ctx, f_ctx, m_ctx, None, *rest)
