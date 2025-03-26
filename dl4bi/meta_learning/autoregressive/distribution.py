346915e6bb48bc0c38fc4692f65e0416a97b2df4
Piotr Grynfelder <piotr@grynfelder.me>
2025-03-26 14:52:25 +0000

    presampled distribution

--- dl4bi/meta_learning/autoregressive/distribution.py
+++ dl4bi/meta_learning/autoregressive/distribution.py
@@ -38,17 +38,61 @@ class ARDistribution(Distribution):
         if sample_shape != ():
             raise NotImplementedError
 
-        return self.sampler.sample(
+        sample, logprobs = self.sampler.sample(
             rng, self.s_ctx, self.f_ctx, self.s_test, strategy=None
         )
+        return sample
 
-    def log_prob(self, f_test):
+    def log_prob(self, value):
         rng = jax.random.seed(0)
         return self.sampler.logpdf(
             rng,  # unused for deterministic sampling strategies
             self.s_ctx,
             self.f_ctx,
             self.s_test,
-            f_test,
+            value,
             strategy=None,
         )
+
+
+class PresampledDistribution(Distribution):
+    """
+    Distribution operating on pre-sampled data.
+
+    The idea is that if we have a distribution thats independent from the rest of the model,
+    then presampling N samples ahead of time `samples = [s_0, ..., s_N-1]`
+    and outputting them in order is the same as sampling within the program.
+
+    To associate logprobs with the samples they are identified with an integer.
+    `sample` will output integers.
+    To get the actual sample value, call `get_value`
+    """
+
+    samples: jax.Array
+    logprobs: jax.Array
+
+    def __init__(
+        self,
+        S,  # [N, ...]
+        logprobs,  # [N]
+        batch_size: int,
+    ):
+        self.samples = S
+        self.logprobs = logprobs
+        self.batch_size = batch_size
+
+        super.__init__(batch_shape=(batch_size,), event_shape=())
+
+    def sample(self, rng, sample_shape=()):
+        if sample_shape != ():
+            raise NotImplementedError
+
+        idx = jnp.arange(self.i, self.i + self.batch_size)
+        self.i += self.batch_size
+        return idx
+
+    def log_prob(self, idx):
+        return self.logprobs[idx]
+
+    def get_value(self, idx):
+        return self.samples[idx]
