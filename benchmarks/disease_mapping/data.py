07eeae85bfb5c053407a39a58b712fb0f8fd75f9
Piotr Grynfelder <piotr@grynfelder.dev>
2025-04-11 15:14:25 +0200

    drop dependency on malariaAtlas

--- benchmarks/disease_mapping/data.py
+++ benchmarks/disease_mapping/data.py
@@ -1,4 +1,3 @@
-from itertools import chain
 from os import environ
 from pathlib import Path
 
@@ -7,7 +6,6 @@ import numpy as np
 import pandas as pd
 import rasterio
 import requests
-from shapely import MultiPolygon, Polygon
 
 from benchmarks.disease_mapping.utils import cartesian_product
 
@@ -32,71 +30,29 @@ def round_to_grid(degrees, res):
     return round_to_multiple(degrees * DEG_TO_SEC, res) / DEG_TO_SEC
 
 
-def multipolygon_r2py(geometry):
-    geometry = chain.from_iterable(geometry)  # flatten
-    geometry = map(np.array, geometry)
-    geometry = map(Polygon, geometry)
-
-    shape = MultiPolygon(geometry)
-    return shape
-
-
-def r_to_gpd(rdf):
-    import rpy2.robjects as ro
-    from rpy2.rinterface_lib.sexp import (
-        NACharacterType,
-        NAComplexType,
-        NAIntegerType,
-        NALogicalType,
-        NARealType,
-    )
-
-    NA = (
-        NACharacterType,
-        NAComplexType,
-        NAIntegerType,
-        NALogicalType,
-        NARealType,
-    )
-
-    df = gpd.GeoDataFrame()
-
-    with ro.default_converter.context():
-        convert = ro.conversion.get_conversion().rpy2py
-        for c in rdf.colnames:
-            match c:
-                case "geometry":
-                    geometry = rdf.rx2(c)
-                    geometry = [multipolygon_r2py(x) for x in geometry]
-                    df = df.set_geometry(geometry)
-                case _:
-                    series = rdf.rx2(c)
-                    series = [
-                        (pd.NA if isinstance(x, NA) else convert(x)) for x in series
-                    ]
-                    df[c] = series
-
-    return df.convert_dtypes()
-
-
 def get_shape(iso: str, region: str | None = None):
     iso = iso.upper()
-    cache_path = CACHE_DIR / f"{iso}_shape.feather"
+    url = f"https://geodata.ucdavis.edu/gadm/gadm4.1/gpkg/gadm41_{iso}.gpkg"
+    file_path = CACHE_DIR / f"{iso}.gpkg"
 
-    if cache_path.exists():
-        df = gpd.read_feather(cache_path)
+    if file_path.exists():
+        print("Reading geodata from cache.")
     else:
-        from rpy2.robjects.packages import importr
+        print(f"Downloading geodata from {url}.")
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError(f"Failed to download data from {url}")
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.write_bytes(response.content)
 
-        df = r_to_gpd(
-            importr("malariaAtlas").getShp(ISO=iso, admin_level=["admin0", "admin1"])
-        )
-        df.to_feather(cache_path)
+    if region is None:
+        df = gpd.read_file(file_path, layer="ADM_ADM_0", engine="pyogrio")
+    else:
+        df = gpd.read_file(file_path, layer="ADM_ADM_1", engine="pyogrio")
+        region_df = df.query("NAME_1==@region")
 
-    if region is not None:
-        region_df = df.query("name_1==@region")
         if region_df.empty:
-            available_regions = sorted(df["name_1"].dropna().unique())
+            available_regions = sorted(df["NAME_1"].dropna().unique())
             raise ValueError(
                 f"Region not found. \nAvailiable regions: {available_regions}."
             )
@@ -185,85 +141,6 @@ def get_survey_data(
     query: str | None = None,
     res: int | None = 150,  # if not None round to grid of given res in seconds
 ):
-    """
-    Loads Pf survey data from malariaAtlas.
-
-    Args:
-        iso: iso country code
-        region: region name (optional)
-        query: query string to filter the data, available columns:
-            'geometry', 'dhs_id', 'site_id', 'site_name', 'latitude', 'longitude',
-            'rural_urban', 'country', 'country_id', 'continent_id', 'month_start',
-            'year_start', 'month_end', 'year_end', 'lower_age', 'upper_age',
-            'examined', 'positive', 'pr', 'species', 'method', 'rdt_type',
-            'pcr_type', 'malaria_metrics_available', 'location_available',
-            'permissions_info', 'citation1', 'citation2', 'citation3'
-        res: if not None, round to grid of given resolution in seconds. Default 150.
-    """
-    cache_path: Path = CACHE_DIR / "Africa_prevalence.feather"
-
-    if cache_path.exists():
-        df = gpd.read_feather(cache_path)
-    else:
-        from rpy2.robjects.packages import importr
-
-        ma = importr("malariaAtlas")
-        # download the data
-        rdf = ma.getPR(continent="Africa", species="Pf")
-        # convert to pandas dataframe
-        df = r_to_gpd(rdf)
-        df = df.dropna(subset=["longitude", "latitude", "positive", "examined"])
-        df = df.set_geometry(
-            gpd.points_from_xy(df.longitude, df.latitude, crs="EPSG:4326")
-        )
-        df.to_feather(cache_path)
-
-    if iso is not None:
-        iso = iso.upper()
-        df = df.query("country_id==@iso")
-
-    if region is not None:
-        shape = get_shape(iso, region)
-        df = df.clip(shape)
-
-    if query:
-        df = df.query(query)
-
-    print(df)
-    print(f"Selected {len(df)} rows.")
-    assert len(df) > 0, "Number of surveys selected must be >0."
-
-    if res is not None:
-        lat = round_to_grid(df.geometry.x, res)
-        lon = round_to_grid(df.geometry.y, res)
-        df = df.set_geometry(gpd.points_from_xy(lon, lat))
-        # merge surveys from points close-by into one
-        df = df.groupby(
-            # since skipping time in s, ignore time for merging
-            ["geometry"],
-            as_index=False,
-        ).sum()
-        # this is now nonsense except for the positive and examined columns
-
-    # skip time for now
-    # t = (df.YY * 12 + df.MM).to_numpy()
-    # t -= t.min()
-    # t = t[..., None]
-
-    s = np.array([[p.x, p.y] for p in df.geometry])
-
-    n_pos = df.positive.to_numpy()
-    n = df.examined.to_numpy()
-
-    return {"s": s, "n_pos": n_pos, "n": n}
-
-
-def get_survey_data2(
-    iso: str | None,
-    region: str | None = None,
-    query: str | None = None,
-    res: int | None = 150,  # if not None round to grid of given res in seconds
-):
     dataset_id = "doi:10.7910/DVN/Z29FR0/FFDQI3"
     url = f"https://dataverse.harvard.edu/api/access/datafile/:persistentId/?persistentId={dataset_id}"
 
@@ -274,7 +151,7 @@ def get_survey_data2(
         pass
     else:
         print(f"Downloading survey data from {url}.")
-        response = requests.get(url, headers={"User-Agent": "pydataverse"})
+        response = requests.get(url)
         if response.status_code != 200:
             raise ValueError(f"Failed to download data from {url}")
         file_path.parent.mkdir(parents=True, exist_ok=True)
