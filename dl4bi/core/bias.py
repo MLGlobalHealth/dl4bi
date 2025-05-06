e4544e69f9f04ec11479191acdb680b563f88fbb
Piotr Grynfelder <piotr@grynfelder.me>
2025-05-06 15:24:18 +0100

    move great_circle_dist to core.sim

--- dl4bi/core/bias.py
+++ dl4bi/core/bias.py
@@ -7,9 +7,8 @@ import flax.linen.initializers as init
 import jax
 import jax.numpy as jnp
 from jax import jit, vmap
-from sps.kernels import _prepare_dims, l2_dist
 
-from dl4bi.core.utils import make_pairwise
+from dl4bi.core.sim import great_circle_dist, l2_dist
 
 
 def init_scalar_bias_params(mod: nn.Module, name: str, num_heads: int):
@@ -135,44 +134,6 @@ def scanned_exponential_network_bias(
     d = vmap(func)(qs_meta, ks_meta)
     mask = jnp.isfinite(d)
     return exponential_network_bias(d, mask, a, b)
-
-
-@jit
-def great_circle_dist(x: jax.Array, y: jax.Array) -> jax.Array:
-    r"""Great circle distance between two [..., 2] arrays.
-    Inputs and outputs are in degrees.
-
-    Args:
-        x: Input array of size `[..., 2]`.
-        y: Input array of size `[..., 2]`.
-
-    Returns:
-        Matrix of all pairwise distances.
-    """
-
-    def d(x, y):
-        # copied from benchmarks.disease_mapping.utils
-        x_lon, x_lat = x
-        y_lon, y_lat = y
-        x_lon, x_lat, y_lon, y_lat = map(jnp.deg2rad, (x_lon, x_lat, y_lon, y_lat))
-
-        d_lon = jnp.abs(x_lon - y_lon)
-
-        sin = jnp.sin
-        cos = jnp.cos
-
-        arc_length = jnp.atan2(
-            jnp.sqrt(
-                (cos(y_lat) * sin(d_lon)) ** 2
-                + (cos(x_lat) * sin(y_lat) - sin(x_lat) * cos(y_lat) * cos(d_lon)) ** 2
-            ),
-            sin(x_lat) * sin(y_lat) + cos(x_lat) * cos(y_lat) * cos(d_lon),
-        )
-
-        return jnp.rad2deg(arc_length)
-
-    x, y = _prepare_dims(x, y)
-    return make_pairwise(d)(x, y)
 
 
 def init_tisa_bias_params(mod: nn.Module, name: str, num_heads: int, num_basis: int):
--- dl4bi/core/sim.py
+++ dl4bi/core/sim.py
@@ -3,7 +3,9 @@ from functools import partial
 import jax
 import jax.numpy as jnp
 from jax import jit
-from sps.kernels import l2_dist
+from sps.kernels import _prepare_dims, l2_dist
+
+from dl4bi.core.utils import make_pairwise
 
 
 @partial(jit, static_argnames=("causal",))
@@ -16,3 +18,41 @@ def delta_time(
     if causal:
         return jnp.where(d <= 0, d, jnp.inf)
     return d  # [Q, R]
+
+
+@jit
+def great_circle_dist(x: jax.Array, y: jax.Array) -> jax.Array:
+    r"""Great circle distance between two [..., 2] arrays.
+    Inputs and outputs are in degrees.
+
+    Args:
+        x: Input array of size `[..., 2]`.
+        y: Input array of size `[..., 2]`.
+
+    Returns:
+        Matrix of all pairwise distances.
+    """
+
+    def d(x, y):
+        # copied from benchmarks.disease_mapping.utils
+        x_lon, x_lat = x
+        y_lon, y_lat = y
+        x_lon, x_lat, y_lon, y_lat = map(jnp.deg2rad, (x_lon, x_lat, y_lon, y_lat))
+
+        d_lon = jnp.abs(x_lon - y_lon)
+
+        sin = jnp.sin
+        cos = jnp.cos
+
+        arc_length = jnp.atan2(
+            jnp.sqrt(
+                (cos(y_lat) * sin(d_lon)) ** 2
+                + (cos(x_lat) * sin(y_lat) - sin(x_lat) * cos(y_lat) * cos(d_lon)) ** 2
+            ),
+            sin(x_lat) * sin(y_lat) + cos(x_lat) * cos(y_lat) * cos(d_lon),
+        )
+
+        return jnp.rad2deg(arc_length)
+
+    x, y = _prepare_dims(x, y)
+    return make_pairwise(d)(x, y)
